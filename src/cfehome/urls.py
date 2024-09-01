8fbf10ed7070d19e676fbb7d5b81876726941486
Rik Smith <riksmith1@hotmail.com>
2024-09-01 12:02:57 +0800

    Prepare for railway

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_page_view
+from .views import home_view, about_view
 
 urlpatterns = [
+    path('', home_view),
+    path('about/', about_view),
+    path('hello-world/', home_view),
+    path('hello-world.html', home_view),
     path('admin/', admin.site.urls),
-    path('', home_page_view),
 ]
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -4,15 +4,23 @@ from django.http import HttpResponse
 from visits.models import PageVisit
 
 
-def home_page_view(request):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = page_qs.count() * 100.0 / qs.count()
+    except:
+        percent = 0
     my_title = 'My Page'
+    html_template = 'home.html'
     my_context = {
         'page_title': my_title,
         'page_visit_count': page_qs.count(),
-        'percent': page_qs.count() * 100.0 / qs.count(),
+        'percent': percent,
         'totol_visit_count': qs.count(),
     }
     PageVisit.objects.create(path=request.path)
-    return render(request, 'home.html', my_context)
+    return render(request, html_template, my_context)
